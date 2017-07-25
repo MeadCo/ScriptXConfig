94fdc45e3c1546dad56f669211a930b56aacae22
Pete Cole <pcole@meadroid.com>
2017-07-25 16:55:00 +0100

    Build against .NET Framework 4.5

--- MeadCo.ScriptXConfigHandler/Properties/AssemblyInfo.cs
+++ MeadCo.ScriptXConfigHandler/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0-beta01")]
+[assembly: AssemblyInformationalVersion("3.0.0-beta02")]
 [assembly: InternalsVisibleTo("MeadCo.ScriptXConfigHandler.Tests")]
 [assembly: InternalsVisibleTo("MeadCo.ScriptXConfigurationHandler.Explorables")]
 
